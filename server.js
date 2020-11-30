1a38a31ec22748426fa000f6871c1861d657c1a0
Antoine <fortyonepointsix@gmail.com>
2020-11-29 20:28:39 -0500

    contracts

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 var express = require("express");
 var path = require("path");
+var onboard = require('bnc-onboard');
+let Web3 = require('web3')
+
 
 const PORT = process.env.PORT || 3030;
 
 let app = express();
+
+
+const onboard = Onboard({
+  dappId: "f86fb323-1c63-488e-8f3b-30b49374dd96",       // [String] The API key created by step one above
+  networkId: 5  // [Integer] The Ethereum network ID your Dapp uses.
+  subscriptions: {
+    wallet: wallet => {
+       web3 = new Web3(wallet.provider)
+    }
+  }
+});
 
 app.use(express.static(__dirname + "/public"))
 
