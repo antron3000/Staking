74d47f949171c8c36fcc4982e796f01c6438ab31
Antoine <fortyonepointsix@gmail.com>
2020-11-30 14:45:37 -0500

    contract update

--- contracts/SharedDeposit.sol
+++ contracts/SharedDeposit.sol
@@ -113,6 +113,223 @@ library SafeMath {
         return a % b;
     }
 }
+
+contract uStakeEth {
+    /// @notice EIP-20 token name for this token
+    string public constant name = "uStake Eth";
+
+    /// @notice EIP-20 token symbol for this token
+    string public constant symbol = "staked ETH";
+
+    /// @notice EIP-20 token decimals for this token
+    uint8 public constant decimals = 18;
+
+    /// @notice Total number of tokens in circulation
+    uint public totalSupply;
+
+    /// @notice Address which may mint new tokens
+    address public minter;
+
+    /// @notice The timestamp after which minting may occur
+    uint public mintingAllowedAfter;
+
+    /// @notice Minimum time between mints
+    // Flash mint protection
+    uint32 public constant minimumTimeBetweenMints = 1;
+
+    // Allowance amounts on behalf of others
+    mapping (address => mapping (address => uint96)) internal allowances;
+
+    // Official record of token balances for each account
+    mapping (address => uint96) internal balances;
+
+    //An event thats emitted when the minter address is changed
+    event MinterChanged(address minter, address newMinter);
+
+    //The standard EIP-20 transfer event
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+
+    //The standard EIP-20 approval event
+    event Approval(address indexed owner, address indexed spender, uint256 amount);
+
+    /**
+     * @notice Construct a new token
+     */
+    constructor() public {
+        minter = msg.sender;
+        emit MinterChanged(address(0), minter);
+        mintingAllowedAfter = block.timestamp;
+    }
+
+    /**
+     * @notice Change the minter address
+     * @param minter_ The address of the new minter
+     */
+    function setMinter(address minter_) external {
+        require(msg.sender == minter, "Uni::setMinter: only the minter can change the minter address");
+        emit MinterChanged(minter, minter_);
+        minter = minter_;
+    }
+
+    /**
+     * @notice Mint new tokens
+     * @param dst The address of the destination account
+     * @param rawAmount The number of tokens to be minted
+     */
+    function mint(address dst, uint rawAmount) external {
+        require(msg.sender == minter, "Uni::mint: only the minter can mint");
+        require(block.timestamp >= mintingAllowedAfter, "Uni::mint: minting not allowed yet");
+        require(dst != address(0), "Uni::mint: cannot transfer to the zero address");
+
+        // record the mint
+        mintingAllowedAfter = SafeMath.add(block.timestamp, minimumTimeBetweenMints);
+
+        // mint the amount
+        uint96 amount = safe96(rawAmount, "Uni::mint: amount exceeds 96 bits");
+        totalSupply = safe96(SafeMath.add(totalSupply, amount), "Uni::mint: totalSupply exceeds 96 bits");
+
+        // transfer the amount to the recipient
+        balances[dst] = add96(balances[dst], amount, "Uni::mint: transfer amount overflows");
+        emit Transfer(address(0), dst, amount);
+    }
+
+         /**
+     * @notice Burn tokens
+     * @param src The address of the origin account
+     * @param rawAmount The number of tokens to be minted
+     */
+
+    function burn(address src, uint rawAmount) external {
+        require(msg.sender == minter, "Uni::burn: only the minter can burn");
+        require(block.timestamp >= mintingAllowedAfter, "Uni::burn: minting not allowed yet");
+        uint96 amount = safe96(rawAmount, "Uni::burn: amount exceeds 96 bits");
+        require(amount <= totalSupply, "Uni::burn: exceededed total supply");
+        mintingAllowedAfter = SafeMath.add(block.timestamp, minimumTimeBetweenMints);
+
+        totalSupply = safe96(SafeMath.sub(totalSupply, amount), "Uni::burn: totalSupply exceeds 96 bits");
+
+        // transfer the amount to the recipient
+        balances[src] = sub96(balances[src], amount, "Uni::burn: transfer amount overflows");
+        emit Transfer(src, address(0), amount);
+    }
+
+    /**
+     * @notice Get the number of tokens `spender` is approved to spend on behalf of `account`
+     * @param account The address of the account holding the funds
+     * @param spender The address of the account spending the funds
+     * @return The number of tokens approved
+     */
+    function allowance(address account, address spender) external view returns (uint) {
+        return allowances[account][spender];
+    }
+
+    /**
+     * @notice Approve `spender` to transfer up to `amount` from `src`
+     * @dev This will overwrite the approval amount for `spender`
+     *  and is subject to issues noted [here](https://eips.ethereum.org/EIPS/eip-20#approve)
+     * @param spender The address of the account which may transfer tokens
+     * @param rawAmount The number of tokens that are approved (2^256-1 means infinite)
+     * @return Whether or not the approval succeeded
+     */
+    function approve(address spender, uint rawAmount) external returns (bool) {
+        uint96 amount;
+        if (rawAmount == uint(-1)) {
+            amount = uint96(-1);
+        } else {
+            amount = safe96(rawAmount, "Uni::approve: amount exceeds 96 bits");
+        }
+
+        allowances[msg.sender][spender] = amount;
+
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    /**
+     * @notice Get the number of tokens held by the `account`
+     * @param account The address of the account to get the balance of
+     * @return The number of tokens held
+     */
+    function balanceOf(address account) external view returns (uint) {
+        return balances[account];
+    }
+
+    /**
+     * @notice Transfer `amount` tokens from `msg.sender` to `dst`
+     * @param dst The address of the destination account
+     * @param rawAmount The number of tokens to transfer
+     * @return Whether or not the transfer succeeded
+     */
+    function transfer(address dst, uint rawAmount) external returns (bool) {
+        uint96 amount = safe96(rawAmount, "Uni::transfer: amount exceeds 96 bits");
+        _transferTokens(msg.sender, dst, amount);
+        return true;
+    }
+
+    /**
+     * @notice Transfer `amount` tokens from `src` to `dst`
+     * @param src The address of the source account
+     * @param dst The address of the destination account
+     * @param rawAmount The number of tokens to transfer
+     * @return Whether or not the transfer succeeded
+     */
+    function transferFrom(address src, address dst, uint rawAmount) external returns (bool) {
+        address spender = msg.sender;
+        uint96 spenderAllowance = allowances[src][spender];
+        uint96 amount = safe96(rawAmount, "Uni::approve: amount exceeds 96 bits");
+
+        if (spender != src && spenderAllowance != uint96(-1)) {
+            uint96 newAllowance = sub96(spenderAllowance, amount, "Uni::transferFrom: transfer amount exceeds spender allowance");
+            allowances[src][spender] = newAllowance;
+
+            emit Approval(src, spender, newAllowance);
+        }
+
+        _transferTokens(src, dst, amount);
+        return true;
+    }
+
+
+
+    function _transferTokens(address src, address dst, uint96 amount) internal {
+        require(src != address(0), "Uni::_transferTokens: cannot transfer from the zero address");
+        require(dst != address(0), "Uni::_transferTokens: cannot transfer to the zero address");
+
+        balances[src] = sub96(balances[src], amount, "Uni::_transferTokens: transfer amount exceeds balance");
+        balances[dst] = add96(balances[dst], amount, "Uni::_transferTokens: transfer amount overflows");
+        emit Transfer(src, dst, amount);
+    }
+
+
+
+    function safe32(uint n, string memory errorMessage) internal pure returns (uint32) {
+        require(n < 2**32, errorMessage);
+        return uint32(n);
+    }
+
+    function safe96(uint n, string memory errorMessage) internal pure returns (uint96) {
+        require(n < 2**96, errorMessage);
+        return uint96(n);
+    }
+
+    function add96(uint96 a, uint96 b, string memory errorMessage) internal pure returns (uint96) {
+        uint96 c = a + b;
+        require(c >= a, errorMessage);
+        return c;
+    }
+
+    function sub96(uint96 a, uint96 b, string memory errorMessage) internal pure returns (uint96) {
+        require(b <= a, errorMessage);
+        return a - b;
+    }
+
+    function getChainId() internal pure returns (uint) {
+        uint256 chainId;
+        assembly { chainId := chainid() }
+        return chainId;
+    }
+}
+
 // ┏━━━┓━┏┓━┏┓━━┏━━━┓━━┏━━━┓━━━━┏━━━┓━━━━━━━━━━━━━━━━━━━┏┓━━━━━┏━━━┓━━━━━━━━━┏┓━━━━━━━━━━━━━━┏┓━
 // ┃┏━━┛┏┛┗┓┃┃━━┃┏━┓┃━━┃┏━┓┃━━━━┗┓┏┓┃━━━━━━━━━━━━━━━━━━┏┛┗┓━━━━┃┏━┓┃━━━━━━━━┏┛┗┓━━━━━━━━━━━━┏┛┗┓
 // ┃┗━━┓┗┓┏┛┃┗━┓┗┛┏┛┃━━┃┃━┃┃━━━━━┃┃┃┃┏━━┓┏━━┓┏━━┓┏━━┓┏┓┗┓┏┛━━━━┃┃━┗┛┏━━┓┏━┓━┗┓┏┛┏━┓┏━━┓━┏━━┓┗┓┏┛
@@ -233,73 +450,6 @@ contract Ownable {
     }
 }
 
-interface IBETH {
-    function setMinter(address minter_) external;
-
-    /**
-     * @notice Mint new tokens
-     * @param dst The address of the destination account
-     * @param rawAmount The number of tokens to be minted
-     */
-    function mint(address dst, uint rawAmount) external;
-
-    function burn(address src, uint rawAmount) external;
-
-    function mintingAllowedAfter() external view returns (uint);
-
-    /**
-     * @notice Get the number of tokens `spender` is approved to spend on behalf of `account`
-     * @param account The address of the account holding the funds
-     * @param spender The address of the account spending the funds
-     * @return The number of tokens approved
-     */
-    function allowance(address account, address spender) external view returns (uint);
-
-    /**
-     * @notice Approve `spender` to transfer up to `amount` from `src`
-     * @dev This will overwrite the approval amount for `spender`
-     *  and is subject to issues noted [here](https://eips.ethereum.org/EIPS/eip-20#approve)
-     * @param spender The address of the account which may transfer tokens
-     * @param rawAmount The number of tokens that are approved (2^256-1 means infinite)
-     * @return Whether or not the approval succeeded
-     */
-    function approve(address spender, uint rawAmount) external returns (bool);
-
-    /**
-     * @notice Triggers an approval from owner to spends
-     * @param owner The address to approve from
-     * @param spender The address to be approved
-     * @param rawAmount The number of tokens that are approved (2^256-1 means infinite)
-     * @param deadline The time at which to expire the signature
-     * @param v The recovery byte of the signature
-     * @param r Half of the ECDSA signature pair
-     * @param s Half of the ECDSA signature pair
-     */
-    function permit(address owner, address spender, uint rawAmount, uint deadline, uint8 v, bytes32 r, bytes32 s) external;
-
-    /**
-     * @notice Get the number of tokens held by the `account`
-     * @param account The address of the account to get the balance of
-     * @return The number of tokens held
-     */
-    function balanceOf(address account) external view returns (uint);
-
-    /**
-     * @notice Transfer `amount` tokens from `msg.sender` to `dst`
-     * @param dst The address of the destination account
-     * @param rawAmount The number of tokens to transfer
-     * @return Whether or not the transfer succeeded
-     */
-    function transfer(address dst, uint rawAmount) external returns (bool);
-    /**
-     * @notice Transfer `amount` tokens from `src` to `dst`
-     * @param src The address of the source account
-     * @param dst The address of the destination account
-     * @param rawAmount The number of tokens to transfer
-     * @return Whether or not the transfer succeeded
-     */
-    function transferFrom(address src, address dst, uint rawAmount) external returns (bool);
-}
 
 
 /**
@@ -345,7 +495,6 @@ contract SharedDeposit is Ownable, ReentrancyGuard {
     using SafeMath for uint256;
     using SafeMath for uint;
      /* ========== STATE VARIABLES ========== */
-    //address goerli_medella_beacon_testnet = 0x07b39F4fDE4A38bACe212b546dAc87C58DfE3fDC; //TODO: change
     address public depositContractAddress = 0x8c5fecdC472E27Bc447696F431E425D02dd46a8c;
     address public BETHTokenAddress;
 
@@ -361,26 +510,21 @@ contract SharedDeposit is Ownable, ReentrancyGuard {
     uint256 public adminFeeTotal;
     //uint256 public pricePerShare;
 
-    IBETH BETHToken;
+    uStakeEth u;
 
     constructor(uint256 _numValidators, uint256 _adminFee, address _BETHTokenAddress) public {
+        u = new uStakeEth();
+
         depositContract = IDepositContract(depositContractAddress);
 
         numValidators = _numValidators;
         adminFee = _adminFee;
         BETHTokenAddress = _BETHTokenAddress;
 
-        BETHToken = IBETH(BETHTokenAddress);
-
         adminFeeTotal = 0;
         curValidatorShares = 0;
         costPerValidator = uint(32).add(adminFee);
         //pricePerShare = numValidators.mul(32).mul(1e18).add(adminFee).div((32).mul(1e18));
-    }
-
-    // VIEW FUNCTIONS
-    function mintingAllowedAfter() external view returns (uint256) {
-        return BETHToken.mintingAllowedAfter();
     }
 
     function maxValidatorShares() external view returns (uint256) {
@@ -403,7 +547,7 @@ contract SharedDeposit is Ownable, ReentrancyGuard {
         require(newShareTotal <= costPerValidator.mul(numValidators).mul(1e18), "Eth2Staker:deposit:Amount too large, not enough validators left");
         curValidatorShares = newShareTotal;
         adminFeeTotal = adminFeeTotal.add(adminFee.mul(shareOfOne));
-        BETHToken.mint(msg.sender, valMinusAdmin);
+        u.mint(msg.sender, valMinusAdmin);
     }
 
     function withdraw(uint256 amount) public nonReentrant {
@@ -415,14 +559,14 @@ contract SharedDeposit is Ownable, ReentrancyGuard {
         require(newShareTotal <= costPerValidator.mul(numValidators).mul(1e18), "Eth2Staker:deposit:Amount too large, not enough validators left");
         require(newShareTotal >= 0, "Eth2Staker:withdraw:Amount too small, not enough validators left");
         require(address(this).balance > amount, "Eth2Staker:withdraw:not enough balancce in contract");
-        require(BETHToken.balanceOf(msg.sender) >= amount, "Eth2Staker: Sender balance not enough");
+        require(u.balanceOf(msg.sender) >= amount, "Eth2Staker: Sender balance not enough");
 
         // Todo : check if deadline has passed or not
         // make sure there is money in the contract
 
         curValidatorShares = newShareTotal;
         adminFeeTotal = adminFeeTotal.sub(adminFee.mul(shareOfOne));
-        BETHToken.burn(msg.sender, amount);
+        u.burn(msg.sender, amount);
         address payable sender = msg.sender;
         sender.transfer(amount);
     }
@@ -441,11 +585,6 @@ contract SharedDeposit is Ownable, ReentrancyGuard {
 
     function setNumValidators(uint256 _numValidators) external onlyOwner {
         numValidators = _numValidators;
-    }
-
-    // Recover ownership of token supply and mint
-    function setMinter(address minter_) external onlyOwner {
-        BETHToken.setMinter(minter_);
     }
 
     function setAdminFee(uint256 amount) external onlyOwner {
