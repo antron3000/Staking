259628b822db3bb166245ea4fa25a061abfd6868
johhonn <35551051+johhonn@users.noreply.github.com>
2020-12-08 23:52:30 -0500

    Update uPool.sol
    change ownable to use tx.origin instead of msg.sender

--- contracts/uPool.sol
+++ contracts/uPool.sol
@@ -380,7 +380,7 @@ contract Ownable {
      * @dev Initializes the contract setting the deployer as the initial owner.
      */
     constructor () {
-        _owner = msg.sender;
+        _owner = tx.origin;
         emit OwnershipTransferred(address(0), _owner);
     }
 
